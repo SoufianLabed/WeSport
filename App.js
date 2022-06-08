b94d75fe0d290b3dc5a600f5cf1053e9588ca5b3
Soufian LABED <labedsoufian@gmail.com>
2022-06-08 23:33:24 +0200

    NEW : BottomBar

--- App.js
+++ App.js
@@ -2,16 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import 'react-native-gesture-handler';
-<<<<<<< HEAD
 import Login from "./src/Screen/Login"
 import RootStackScreen from './RootStackScreen';
 import StackScreen from './HomeStackScreen';
-=======
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeStackScreen from './HomeStackScreen';
-
->>>>>>> ef1091cc8e80f63870d68574543ce3fbf735e3de
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
