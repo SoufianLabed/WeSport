c6b599c8f38532778996e5ad928ca13aad26722f
nahidath <36660735+nahidath@users.noreply.github.com>
2022-06-21 10:55:13 -0400

    swiperStarter design done

--- src/Screen/SwiperStarter.js
+++ src/Screen/SwiperStarter.js
@@ -1,50 +1,68 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet, Button, Pressable} from 'react-native';
 
 import Onboarding from 'react-native-onboarding-swiper';
+import Feather from "react-native-vector-icons/Feather";
 
 const Dots = ({selected}) => {
     let backgroundColor;
 
-    backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+    backgroundColor = selected ? '#3C5BAA' : '#F5F9FA';
 
     return (
-        <View 
+        <View
             style={{
-                width:5,
-                height: 6,
                 marginBottom: 650,
                 marginHorizontal: 3,
-                backgroundColor
+                width: 10,
+                height: 10,
+                borderRadius: 10 / 2,
+                borderColor: '#3C5BAA',
+                borderWidth : 1,
+                backgroundColor,
+                marginTop : 80
             }}
         />
     );
 }
 
 const Skip = ({...props}) => (
-    <TouchableOpacity
-        style={{marginHorizontal:10}}
-        {...props}
-    >
-        <Text style={{fontSize:16}}>Login</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+
+
+        <TouchableOpacity
+            style={styles.footerSwp}
+            {...props}
+        >
+            <Text style={{color : '#3C5BAA', fontSize: 17, fontWeight: 'bold'}}>Skip</Text>
+        </TouchableOpacity>
+    </View>
+
 );
 
 const Next = ({...props}) => (
-    <TouchableOpacity
-        style={{marginHorizontal:10}}
-        {...props}
-    >
-        <Text style={{fontSize:16}}>Next</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+        <TouchableOpacity
+            style={styles.footerSwp}
+            {...props}
+        >
+            <Text style={{color : '#3C5BAA', fontSize: 17, fontWeight: 'bold'}}>Next
+            <Feather
+                name='arrow-right'
+                color='#3C5BAA'
+                size={17}
+            />
+            </Text>
+        </TouchableOpacity>
+    </View>
 );
 
 const Done = ({...props}) => (
     <TouchableOpacity
-        style={{marginHorizontal:10}}
+        style={{backgroundColor : '#F5F9FA', width : 360, padding : 20, justifyContent :'flex-end', alignItems:'flex-end'}}
         {...props}
     >
-        <Text style={{fontSize:16}}>Done</Text>
+        <Text style={{color : '#3C5BAA', fontSize: 17, fontWeight: 'bold', right: 20, bottom: 5}}>Commencer</Text>
     </TouchableOpacity>
 );
 
@@ -57,22 +75,24 @@ const SwiperStarter = ({navigation}) => {
         DotComponent={Dots}
         onSkip={() => navigation.replace("Login")}
         onDone={() => navigation.navigate("Login")}
+        titleStyles={styles.titleOB}
+        subTitleStyles={styles.subtitleOB}
         pages={[
           {
             backgroundColor: '#F5F9FA',
-            image: <Image source={require('../../images/placeholder-.png')} style={{marginTop:-300}}/>,
-            title: 'Retrouve des sportifs',
-            subtitle: 'Trouve des événements ou créer les !',
+            image: <Image source={require('../../images/sport-climat-visuel.jpg')} style={styles.imgSwp}/>,
+            title: 'Retrouve des sportifs !',
+            subtitle: 'Trouve des événements ou crée-les !',
           },
           {
             backgroundColor: '#F5F9FA',
-            image: <Image source={require('../../images/placeholder-.png')} style={{marginTop:-300}}/>,
+            image: <Image source={require('../../images/placeholder-.png')} style={styles.imgSwp}/>,
             title: 'Séléctionne les endroits',
             subtitle: 'Près de chez toi ou renseigne une ville précise',
           },
           {
             backgroundColor: '#F5F9FA',
-            image: <Image source={require('../../images/placeholder-.png')} style={{marginTop:-300}}/>,
+            image: <Image source={require('../../images/placeholder-.png')} style={styles.imgSwp}/>,
             title: 'Envoi un message',
             subtitle: "Un événement te plaît, envoi un message",
           },
@@ -85,8 +105,35 @@ export default SwiperStarter;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    backgroundColor : '#F5F9FA',
+    width : 180
   },
+  imgSwp: {
+      height : 391,
+      top : 0,
+      width : '100%',
+      marginTop:-300
+  },
+  titleOB: {
+      color :'#091833',
+      fontWeight : "bold",
+      fontSize : 25,
+      top : 20
+  },
+  subtitleOB: {
+      color : '#091833',
+      fontSize : 16,
+      top : 50
+
+  },
+  footerSwp: {
+      color : '#3C5BAA',
+      padding : 15,
+      left: 50,
+
+
+
+  },
+
 });
